2abb435b7204958a3ead726e2576d646c6a76e00
Jakub Broncel <jakubbroncel@new-host-4.home>
2026-03-26 21:17:06 +0100

    Mac tez dziala

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Widze projekt! Test C++ na Windowsie i Macu." << std::endl;
-    std::cout << "JakubTOGej" << std::endl;
+    std::cout << "JakubTO NIE JESTT Gej!!!" << std::endl;
     return 0;
 }
